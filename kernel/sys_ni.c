d8b52b30bb44b2be07a2eec047806903272710a2
Oleksandr Natalenko <oleksandr@natalenko.name>
2021-10-04 22:58:59 +0200

    mm: wire up pmadv_ksm system call
    Present `pmadv_ksm()` syscall for all the arches.
    
    Signed-off-by: Oleksandr Natalenko <oleksandr@natalenko.name>
    Signed-off-by: Simão Gomes Viana <simao@halogenos.org>

--- kernel/sys_ni.c
+++ kernel/sys_ni.c
@@ -292,6 +292,7 @@ COND_SYSCALL(mincore);
 COND_SYSCALL(madvise);
 COND_SYSCALL(process_madvise);
 COND_SYSCALL(process_mrelease);
+COND_SYSCALL(pmadv_ksm);
 COND_SYSCALL(remap_file_pages);
 COND_SYSCALL(mbind);
 COND_SYSCALL(get_mempolicy);
