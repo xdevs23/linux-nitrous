895dfcb9d3747d496be0f361d7184e609a35816e
Sascha Hauer <s.hauer@pengutronix.de>
2022-05-18 10:23:15 +0200

    rtw88: Add rtw8723du chipset support
    Add support for the rtw8723du chipset based on
    https://github.com/ulli-kroll/rtw88-usb.git
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ drivers/net/wireless/realtek/rtw88/rtw8723du.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
+/* Copyright(c) 2018-2019  Realtek Corporation
+ */
+
+#ifndef __RTW_8723DU_H_
+#define __RTW_8723DU_H_
+
+/* USB Vendor/Product IDs */
+#define RTW_USB_VENDOR_ID_REALTEK		0x0BDA
+
+extern struct rtw_chip_info rtw8723d_hw_spec;
+
+#endif
