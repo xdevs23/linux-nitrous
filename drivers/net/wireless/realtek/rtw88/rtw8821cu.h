fa78a63067fc71c28d551028e43c6d31562df567
Sascha Hauer <s.hauer@pengutronix.de>
2022-05-18 10:23:16 +0200

    rtw88: Add rtw8821cu chipset support
    Add support for the rtw8821cu chipset based on
    https://github.com/ulli-kroll/rtw88-usb.git
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ drivers/net/wireless/realtek/rtw88/rtw8821cu.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
+/* Copyright(c) 2018-2019  Realtek Corporation
+ */
+
+#ifndef __RTW_8821CU_H_
+#define __RTW_8821CU_H_
+
+/* USB Vendor/Product IDs */
+#define RTW_USB_VENDOR_ID_REALTEK		0x0BDA
+#define RTW_USB_PRODUCT_ID_REALTEK_8811C	0xC811
+#define RTW_USB_PRODUCT_ID_REALTEK_8821C	0xC81C
+
+extern struct rtw_chip_info rtw8821c_hw_spec;
+
+#endif
