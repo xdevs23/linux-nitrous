d9193388a3612ed504f212945f58ed41db4f3bfe
Sascha Hauer <s.hauer@pengutronix.de>
2022-05-18 10:23:17 +0200

    rtw88: Add rtw8822bu chipset support
    Add support for the rtw8822bu chipset based on
    https://github.com/ulli-kroll/rtw88-usb.git
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Simão Gomes Viana <simao@halogenos.org>

---
+++ drivers/net/wireless/realtek/rtw88/rtw8822bu.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
+/* Copyright(c) 2018-2019  Realtek Corporation
+ */
+
+#ifndef __RTW_8822BU_H_
+#define __RTW_8822BU_H_
+
+/* USB Vendor/Product IDs */
+#define RTW_USB_VENDOR_ID_REALTEK		0x0BDA
+#define RTW_USB_PRODUCT_ID_REALTEK_8812B	0xB812
+#define RTW_USB_PRODUCT_ID_REALTEK_8822B	0xB82C
+
+extern struct rtw_chip_info rtw8822b_hw_spec;
+
+#endif
