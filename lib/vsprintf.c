437b5047fd459ffd8e7255b15e788d63b085d2cc
Simão Gomes Viana <simao@halogenos.org>
2022-10-04 20:48:43 +0200

    lib: add CONFIG_KPTR_RESTRICT
    This allows us to configure it via defconfig
    instead of hardcoding it
    
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>
    Signed-off-by: Simão Gomes Viana <simao@halogenos.org>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -846,7 +846,7 @@ static char *default_pointer(char *buf, char *end, const void *ptr,
 	return ptr_to_id(buf, end, ptr, spec);
 }
 
-int kptr_restrict __read_mostly;
+int kptr_restrict __read_mostly = CONFIG_KPTR_RESTRICT;
 
 static noinline_for_stack
 char *restricted_pointer(char *buf, char *end, const void *ptr,
