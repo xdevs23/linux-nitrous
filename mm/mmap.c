2bc962bc3d61cda4cd9940e0dc669c39e4685f1a
Rik van Riel <riel@surriel.com>
2024-12-19 15:32:53 -0500

    mm: remove unnecessary calls to lru_add_drain
    There seem to be several categories of calls to lru_add_drain
    and lru_add_drain_all.
    
    The first are code paths that recently allocated, swapped in,
    or otherwise processed a batch of pages, and want them all on
    the LRU. These drain pages that were recently allocated,
    probably on the local CPU.
    
    A second category are code paths that are actively trying to
    reclaim, migrate, or offline memory. These often use lru_add_drain_all,
    to drain the caches on all CPUs.
    
    However, there also seem to be some other callers where we
    aren't really doing either. They are calling lru_add_drain(),
    despite operating on pages that may have been allocated
    long ago, and quite possibly on different CPUs.
    
    Those calls are not likely to be effective at anything but
    creating lock contention on the LRU locks.
    
    Remove the lru_add_drain calls in the latter category.
    
    Signed-off-by: Rik van Riel <riel@surriel.com>
    Suggested-by: David Hildenbrand <david@redhat.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- mm/mmap.c
+++ mm/mmap.c
@@ -1664,7 +1664,6 @@ void exit_mmap(struct mm_struct *mm)
 		goto destroy;
 	}
 
-	lru_add_drain();
 	flush_cache_mm(mm);
 	tlb_gather_mmu_fullmm(&tlb, mm);
 	/* update_hiwater_rss(mm) here? but nobody should be looking */
@@ -2107,7 +2106,6 @@ int relocate_vma_down(struct vm_area_struct *vma, unsigned long shift)
 				       vma, new_start, length, false, true))
 		return -ENOMEM;
 
-	lru_add_drain();
 	tlb_gather_mmu(&tlb, mm);
 	next = vma_next(&vmi);
 	if (new_end > old_start) {
