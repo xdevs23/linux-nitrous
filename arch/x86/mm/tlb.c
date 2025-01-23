62dc91b95ec47f79d7fc306873ab2525961286dc
Rik van Riel <riel@surriel.com>
2025-01-22 23:23:26 -0500

    x86/mm: use INVLPGB in flush_tlb_all
    The flush_tlb_all() function is not used a whole lot, but we might
    as well use broadcast TLB flushing there, too.
    
    Signed-off-by: Rik van Riel <riel@surriel.com>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- arch/x86/mm/tlb.c
+++ arch/x86/mm/tlb.c
@@ -1036,6 +1036,19 @@ void flush_tlb_mm_range(struct mm_struct *mm, unsigned long start,
 }
 
 
+static bool broadcast_flush_tlb_all(void)
+{
+	if (!IS_ENABLED(CONFIG_X86_BROADCAST_TLB_FLUSH))
+		return false;
+
+	if (!cpu_feature_enabled(X86_FEATURE_INVLPGB))
+		return false;
+
+	guard(preempt)();
+	invlpgb_flush_all();
+	return true;
+}
+
 static void do_flush_tlb_all(void *info)
 {
 	count_vm_tlb_event(NR_TLB_REMOTE_FLUSH_RECEIVED);
@@ -1044,6 +1057,8 @@ static void do_flush_tlb_all(void *info)
 
 void flush_tlb_all(void)
 {
+	if (broadcast_flush_tlb_all())
+		return;
 	count_vm_tlb_event(NR_TLB_REMOTE_FLUSH);
 	on_each_cpu(do_flush_tlb_all, NULL, 1);
 }
