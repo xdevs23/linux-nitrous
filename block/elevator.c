685c4bc95d99020cd0ea6dd85c97b6f410667f5d
Jan Alexander Steffens (heftig) <heftig@archlinux.org>
2025-08-02 04:36:06 +0200

    block: Clean up elevator_set_default
    In case of a multi-queue device, the code pointlessly loaded the
    default elevator just to drop it again.
    
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- block/elevator.c
+++ block/elevator.c
@@ -733,17 +733,18 @@ void elevator_set_default(struct request_queue *q)
 	 * have multiple queues or mq-deadline is not available, default
 	 * to "none".
 	 */
+	if (q->nr_hw_queues != 1 && !blk_mq_is_shared_tags(q->tag_set->flags))
+		return;
+
 	e = elevator_find_get(ctx.name);
 	if (!e)
 		return;
 
-	if ((q->nr_hw_queues == 1 ||
-			blk_mq_is_shared_tags(q->tag_set->flags))) {
-		err = elevator_change(q, &ctx);
-		if (err < 0)
-			pr_warn("\"%s\" elevator initialization, failed %d, falling back to \"none\"\n",
-					ctx.name, err);
-	}
+	err = elevator_change(q, &ctx);
+	if (err < 0)
+		pr_warn("\"%s\" elevator initialization, failed %d, falling back to \"none\"\n",
+				ctx.name, err);
+
 	elevator_put(e);
 }
 
