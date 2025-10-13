8b820badfb98fe915500a75ec85cf84c0f71483f
Simão Gomes Viana <devel@superboring.dev>
2025-10-13 18:38:44 +0200

    Update default.nix to 6.17.2-4

--- default.nix
+++ default.nix
@@ -96,7 +96,7 @@
   config = {
     boot.kernelPackages = lib.mkOverride 80 (
       let
-        version = "6.17.2-3";
+        version = "6.17.2-4";
         linuxVersion = lib.head (lib.splitString "-" version);
         suffix = "nitrous";
         llvm = pkgs.unstable.llvmPackages_20;
@@ -133,7 +133,7 @@
 
               src = fetchurl {
                 url = "https://gitlab.com/xdevs23/linux-nitrous/-/archive/v${version}/linux-nitrous-v${version}.tar.gz";
-                hash = "0vi96lnm29m45g02gb87yjiz2rxc19k5grdi2jx836qc7fp8ih6d";
+                hash = "15cx13wl6pcrqqgsfry2wzw5bnr091hcbvb5cvpsfr09hwwgc6pk";
               };
 
               structuredExtraConfig = with lib.kernel; {
@@ -184,7 +184,7 @@
                   owner = "amkillam";
                   repo = "ryzen_smu";
                   rev = "172c316f53ac8f066afd7cb9e1da517084273368";
-                  hash = "0vi96lnm29m45g02gb87yjiz2rxc19k5grdi2jx836qc7fp8ih6d";
+                  hash = "15cx13wl6pcrqqgsfry2wzw5bnr091hcbvb5cvpsfr09hwwgc6pk";
                 };
                 monitor-cpu = llvm.stdenv.mkDerivation {
                   pname = "monitor-cpu";
