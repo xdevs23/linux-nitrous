27787583b85e0709ab0107766678c2a16ce38e93
Simão Gomes Viana <devel@superboring.dev>
2025-11-05 00:42:24 +0100

    Update default.nix to 6.17.7-1

--- default.nix
+++ default.nix
@@ -96,7 +96,7 @@
   config = {
     boot.kernelPackages = lib.mkOverride 80 (
       let
-        version = "6.17.6-1";
+        version = "6.17.7-1";
         linuxVersion = lib.head (lib.splitString "-" version);
         suffix = "nitrous";
         llvm = pkgs.llvmPackages_20;
@@ -133,7 +133,7 @@
 
               src = fetchurl {
                 url = "https://gitlab.com/xdevs23/linux-nitrous/-/archive/v${version}/linux-nitrous-v${version}.tar.gz";
-                hash = "sha256-kdZ6+hirh8B/xbzZxfsBDXu8L+XECHOLN2HJFpuIUEY=";
+                hash = "sha256-vDGZ78/VtZka4qGLCa4tlZaDDjtwuV2oS4DgP9DmL08=";
               };
 
               structuredExtraConfig = with lib.kernel; {
